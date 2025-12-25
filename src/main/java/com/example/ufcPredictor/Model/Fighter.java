e60ba6e3d71007a5186a39fd25f6fe6f3e8fda04
Ryan Pugh <rpugh777@yahoo.com>
2025-12-24 19:47:37 -0800

    First commit

---
+++ src/main/java/com/example/ufcPredictor/Model/Fighter.java
@@ -0,0 +1,63 @@
+package com.example.ufcPredictor.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "fighter")
+public class Fighter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //IDENTITY Uses the database's built-in auto-increment feature 
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "height")
+    private double height;
+
+    @Column(name = "reach")
+    private double reach;
+
+    @Column(name = "record")
+    private String fighterRecord;
+
+    @Column(name = "strikes")
+    private double strikesPerMin;
+
+    @Column(name = "takeDownD")
+    private double takeDownDefence;
+
+    @Column(name = "takeDownA")
+    private double takeDownAccuracy;
+
+    @Column(name = "winStreak")
+    private int winStreak;
+
+    @Column(name = "weightClass")
+    private String weightClass;
+
+    @Column(name = "rounds")
+    private int rounds;
+
+    @Column(name = "shortNotice")
+    private boolean shortNotice;
+
+    @Column(name = "country")
+    private String country;
+
+}
---
+++ src/test/java/com/example/ufcPredictor/UfcPredictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ufcPredictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfcPredictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
