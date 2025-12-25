871836582bc859e71afd5e1d242a6da4f8544a40
Ryan Pugh <rpugh777@yahoo.com>
2025-12-24 21:24:51 -0800

    Added fighter repository

---
+++ src/main/java/com/example/ufcPredictor/Repository/FighterRepository.java
@@ -0,0 +1,11 @@
+package com.example.ufcPredictor.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.ufcPredictor.Model.Fighter;
+
+@Repository
+public interface FighterRepository extends JpaRepository<Fighter, Long>{
+
+}
