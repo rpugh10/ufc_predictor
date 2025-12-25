b173cc7c5bf65d77bce48baa46e61f7855e46402
Ryan Pugh <rpugh777@yahoo.com>
2025-12-25 14:26:52 -0800

    Added a custom varible to find fighter names

--- src/main/java/com/example/ufcPredictor/Repository/FighterRepository.java
+++ src/main/java/com/example/ufcPredictor/Repository/FighterRepository.java
@@ -1,5 +1,7 @@
 package com.example.ufcPredictor.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.example.ufcPredictor.Model.Fighter;
 
 @Repository
 public interface FighterRepository extends JpaRepository<Fighter, Long>{
-
+    Optional<Fighter> findByName(String name);
 }
