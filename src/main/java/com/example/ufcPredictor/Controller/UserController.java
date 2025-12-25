3993287d84e40369b648a94d58570c191cb8e988
Ryan Pugh <rpugh777@yahoo.com>
2025-12-25 14:25:14 -0800

    Issues with Put and Delete requestes working to resolve them

---
+++ src/main/java/com/example/ufcPredictor/Controller/UserController.java
@@ -0,0 +1,60 @@
+package com.example.ufcPredictor.Controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ufcPredictor.Model.User;
+import com.example.ufcPredictor.Service.UserService;
+
+
+@RestController
+@RequestMapping("/users") //Class mapping
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        User user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/all")
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @PathVariable User info){
+        User user = userService.updateUser(id, info);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/create")
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    
+
+}
