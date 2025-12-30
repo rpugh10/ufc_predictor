02cb42e66e3669c52af3689d6b4ecbf23a90863d
Ryan Pugh <rpugh777@yahoo.com>
2025-12-30 13:30:37 -0800

    Created userDTO class

---
+++ src/main/java/com/example/ufcPredictor/DTO/UserDTO.java
@@ -0,0 +1,11 @@
+package com.example.ufcPredictor.DTO;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+
+    private String username;
+    private String password;
+    private String confimPassword;
+}
