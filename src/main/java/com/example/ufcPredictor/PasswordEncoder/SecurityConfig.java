45b1bb707391a2d596abd8610673ec49772f5c6a
Ryan Pugh <rpugh777@yahoo.com>
2025-12-30 13:31:35 -0800

    Added password encryption and allowing some http endpoints to pass security

---
+++ src/main/java/com/example/ufcPredictor/PasswordEncoder/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.example.ufcPredictor.PasswordEncoder;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf(csrf -> csrf.disable()) // important for fetch POSTs
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(
+                    "/", 
+                    "/login.css",
+                    "/login.html",
+                    "/createAccount.html",
+                    "/createAccount.css",
+                    "/main.html",
+                    "/main.css",
+                    "/login.js",
+                    "/createAccount.js",
+                    "/users/create"
+                ).permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin(form -> form.disable()); // disable default login page
+
+        return http.build();
+    }
+    
+}
