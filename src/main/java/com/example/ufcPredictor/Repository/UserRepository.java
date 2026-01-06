fd8e64de501b1bec8594799ce9316e1095f5fd84
Ryan Pugh <rpugh777@yahoo.com>
2026-01-05 22:18:24 -0800

    Added findByUsername method

--- src/main/java/com/example/ufcPredictor/Repository/UserRepository.java
+++ src/main/java/com/example/ufcPredictor/Repository/UserRepository.java
@@ -1,11 +1,15 @@
 package com.example.ufcPredictor.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.ufcPredictor.Model.User;
 
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
     boolean existsByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
