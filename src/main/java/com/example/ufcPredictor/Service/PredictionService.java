3076389911ae0241bd3d0b49da9ff6c0257bf416
Ryan Pugh <rpugh777@yahoo.com>
2026-01-06 23:37:31 -0800

    Hard coded prediction logic

--- src/main/java/com/example/ufcPredictor/Service/PredictionService.java
+++ src/main/java/com/example/ufcPredictor/Service/PredictionService.java
@@ -2,7 +2,16 @@ package com.example.ufcPredictor.Service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.ufcPredictor.DTO.PredictionDTO;
+import com.example.ufcPredictor.DTO.PredictionResultDTO;
+
 @Service
 public class PredictionService {
 
+    public PredictionResultDTO predict(PredictionDTO predictionDTO){
+        String winner = predictionDTO.getFighterA();
+        double confidence = 0.99;
+
+        return new PredictionResultDTO(winner, confidence);
+    }
 }
