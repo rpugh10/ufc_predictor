4016d5fce94ba83ff61ab36a40661ad2fb5408a3
Ryan Pugh <rpugh777@yahoo.com>
2026-01-07 23:02:00 -0800

    Added endpoints for predict and fighters/search

--- src/main/java/com/example/ufcPredictor/Controller/PredictionController.java
+++ src/main/java/com/example/ufcPredictor/Controller/PredictionController.java
@@ -1,18 +1,22 @@
 package com.example.ufcPredictor.Controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.ufcPredictor.DTO.PredictionDTO;
-import com.example.ufcPredictor.DTO.PredictionResultDTO;
+import com.example.ufcPredictor.DTO.PredictionRequestDTO;
 import com.example.ufcPredictor.Service.PredictionService;
 
 
 @RestController
-@RequestMapping("/predict")
+@CrossOrigin(origins = {
+    "http://localhost:8080",
+    "http://127.0.0.1:8080"
+})
 public class PredictionController {
 
     private final PredictionService predictionService;
@@ -21,11 +25,13 @@ public class PredictionController {
         this.predictionService = predictionService;
     }
 
-    @PostMapping
-    public ResponseEntity<PredictionResultDTO> predict(@RequestBody PredictionDTO dto){
-        return ResponseEntity.ok(predictionService.predict(dto));
+    @PostMapping("/predict")
+    public ResponseEntity<?> predict(@RequestBody PredictionRequestDTO request) {
+        return predictionService.predict(request.getFighterA(), request.getFighterB());
     }
-    
 
-
+    @GetMapping("/fighters/search")
+    public ResponseEntity<String> search(@RequestParam String q) {
+        return predictionService.searchFighters(q);
+    }
 }
