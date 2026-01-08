d22058b506c24773ba449deca2335b84be5f1c5f
Ryan Pugh <rpugh777@yahoo.com>
2026-01-07 23:06:55 -0800

    Added more endpoints to bypass security

--- src/main/java/com/example/ufcPredictor/PasswordEncoder/SecurityConfig.java
+++ src/main/java/com/example/ufcPredictor/PasswordEncoder/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.ufcPredictor.PasswordEncoder;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,6 +21,7 @@ public class SecurityConfig {
 
         http
             .csrf(csrf -> csrf.disable()) // important for fetch POSTs
+            .cors(Customizer.withDefaults())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
                     "/", 
@@ -35,7 +37,8 @@ public class SecurityConfig {
                     "/users/login",
                     "/predict",
                     "/ufc-background.jpg",
-                    "/main.js"
+                    "/main.js",
+                    "/fighters/search"
                 ).permitAll()
                 .anyRequest().authenticated()
             )
